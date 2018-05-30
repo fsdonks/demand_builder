0023f9eeadc19a2bb8bc165c8c35dece9979013a
mpavlak1 <mpavlak@gmu.edu>
2018-05-30 15:52:00 -0400

    Need to override some stuff in Jfreecharts
    Need to override how the start and end points of a line are calculated for categorical plot line annotations.

--- src/demand_builder/peak.clj
+++ src/demand_builder/peak.clj
@@ -1,8 +1,8 @@
 (ns demand_builder.peak
   (:require [incanter core charts]
-            [demand_builder [chart :as c]])
-  (:import org.jfree.chart.annotations.CategoryLineAnnotation))
-
+            [demand_builder [chart :as c]]
+            [spork.util [table :as t]])
+  (:import  [org.jfree.chart.annotations CategoryAnnotation CategoryLineAnnotation TextAnnotation]))
 ;;Name space for building peak demand stacked bar charts. 
 ;; ***Input file will be different from the output from demand builder (demand output) ***
 ;;    The expected file format contains columns for SRC, AC, RC, NG, and peak
@@ -16,26 +16,57 @@
 ;;  can only draw a line between/across catagories because of the level of abstraction in jfree.chart.annotations 
 (defn add-category-line [chart category value]
   (.addAnnotation (.getCategoryPlot chart)
-    (org.jfree.chart.annotations.CategoryLineAnnotation. category value category value (java.awt.Color/black) (java.awt.BasicStroke. 5 2 2))))
+    (org.jfree.chart.annotations.CategoryLineAnnotation. category value category value 
+      (java.awt.Color/black) (java.awt.BasicStroke. 12 2 2 1 (float-array 3 1) 1))))
+
+;;Need to find a way to change how CategoryLineAnnotations works.
+;;When given a two categories, draws a line from the middle of the first category to the middle of the second.
+;;Have to change this to accept a single category and draw the line from the the category start to the category end.
+;;These values can be found by calling (.getCategoryStart/End categoryPlot i i-count (java.awt.Rectange. 500 400) (.getDomainAxisEdge categoryPlot))
+;; which returns a double. 
+
+;;Still have to find a way to change the end/start location on the line annotations, don't know which draw method has to be overriden
+;; sources: http://www.jfree.org/jfreechart/api/javadoc/org/jfree/chart/annotations/CategoryLineAnnotation.html
+;;          http://www.jfree.org/jfreechart/api/javadoc/org/jfree/chart/plot/
+;;Still need to find out what method the draw method of CategoryAnnotation uses internally. 
+;; I assume internally it calls find middle on category c1 and category c2 then calls someother draw inherited draw function.
+(defn annotation [chart c1 v1]
+  (let [cp (.getCategoryPlot chart)
+        paint (java.awt.Color/black)
+        stroke (java.awt.BasicStroke. 2 2 2)
+        ra (java.awt.Rectangle. 500 400)
+        da (.getDomainAxis cp)
+        va (.getRangeAxis cp)
+        c2 c1 v2 v1]
+    (proxy [org.jfree.chart.annotations.CategoryLineAnnotation] [c1 v1 c2 v2 paint stroke]
+      (draw [g2 cp ra da va]
+        ;(println "DRAWING") ;;need to get it to set as bar edge start and bar edge end
+        (proxy-super draw g2 cp ra da va)))))
 
 (defn add-peaks [chart map-list]
   (let [srcs (flatten [[""] (map #(:SRC %) map-list) [" "]])]
     (doseq [i (range 1 (dec (count srcs)))
-            :let [peak (if (<= i (count map-list)) (c/read-num (:peak (nth map-list (dec i)))) 0)
+            :let [peak (if (<= i (count map-list)) (:peak (nth map-list (dec i))) 0)
                   pre (if (> 0 (dec i)) (nth srcs i) (nth srcs (dec i)))
                   post (if (<= (count srcs) (inc i)) (nth srcs i) (nth srcs (inc i)))
                   m (nth map-list (dec i))
-                  demand (apply + [(c/read-num (:AC m)) (c/read-num (:RC m)) (c/read-num (:NG m))])
-                  color (if (< demand peak) (java.awt.Color. 205 0 0) (java.awt.Color. 0 205 0))]] 
-      (.addAnnotation (.getCategoryPlot chart)
-        (org.jfree.chart.annotations.CategoryLineAnnotation. pre peak post peak color (java.awt.BasicStroke. 1.75 2 2))))))
+                  demand (apply + [(:AC m) (:RC m) (:NG m)])
+                  color (if (< demand peak) (java.awt.Color. 205 0 0) (java.awt.Color. 0 205 0))]]
+      (.addAnnotation (.getCategoryPlot chart) (annotation chart (nth srcs i) peak))))) 
+
+      
+      ;;(.addAnnotation (.getCategoryPlot chart)
+       ;; (org.jfree.chart.annotations.CategoryLineAnnotation. pre peak post peak color 
+       ;;   (java.awt.BasicStroke. 2 2 2 10 (float-array [1 4]) 0))))
+;(comment (float-array [0.00001 20 10]))
+
 
 ;;Creates the stacked bar plot and adds the peak lines for each SRC
 ;;Returns the chart object
 (defn build-peak-chart [map-list & {:keys [title]}]
   (let [chart (incanter.charts/stacked-bar-chart 
                 (flatten [["" "" ""] (map #(c/rep (:SRC %) 3) map-list) [" " " " " "]]) ;; categories
-                (flatten [[0 0 0] (for [m map-list] [(c/read-num (:AC m)) (c/read-num (:RC m)) (c/read-num (:NG m))]) [0 0 0]]) ;;supply (y-values)
+                (flatten [[0 0 0] (for [m map-list] [(:AC m) (:RC m) (:NG m)]) [0 0 0]]) ;;supply (y-values)
                 :group-by (flatten (c/rep ["AC" "RC" "NG"] (+ 2 (count (set (map #(:SRC %) map-list)))))) ;;group-by
                 :legend true :x-label "SRC" :y-label "Number of units" :title title)
         font (java.awt.Font. "Tahoma" 0 10)
@@ -56,3 +87,15 @@
     (when view (incanter.core/view chart))
     chart))
 
+(comment
+  (defn temp [c1 v1 c2 v2 paint stroke]
+    (proxy [org.jfree.chart.annotations.CategoryLineAnnotation] [c1 v1 c2 v2 paint stroke] 
+      (draw [g2 plot dataArea domainAxis rangeAxis]
+        (proxy-super draw g2 plot dataArea domainAxis rangeAxis)))))
+;;draw(Graphics2D g2, CategoryPlot plot, Rectangle2D dataArea, ^CategoryAxis domainAxis, ^ValueAxis rangeAxis)
+
+
+
+;(into [] (t/tabdelimited->records filename))
+;tabdelimited->records
+;"C:\\Users\\michael.m.pavlak.civ\\Documents\\test.txt"

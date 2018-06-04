9fa21f0044c8100b587d687f88a25fe226b667b5
mpavlak1 <mpavlak@gmu.edu>
2018-06-04 10:05:07 -0400

    Fixed read-num and file->map-list
    Replaced read-num and file->map-list with stuff from spork.
    Also fixed cleaned up path name stuff with spork.io

--- src/demand_builder/chart.clj
+++ src/demand_builder/chart.clj
@@ -1,6 +1,6 @@
 (ns demand_builder.chart
   (:require [incanter core charts]
-            [spork.util [table :as t]]
+            [spork.util table parsing]
             [demand_builder [jfreeoverride :as jf]]))
 
 
@@ -9,31 +9,21 @@
 
 ;;Specific function to create sand charts for demand from file 
 ;;  ***uses specific file format, works as of May 2018***
-;;     changes to demand file will require (ONLY) demand-file->sand-charts to be updated
+;;     changes to demand file will require (ONLY) demand-file->sand-charts to be updated (update schema)
 
-;;Function to parse strings with exta syntax and convert to int without throwing error
+;;Function to parse strings as ints (can handle commas)
 (defn read-num [string]
-  (try
-    (if string
-      (let [num (fn [string] (if (= string (re-matches #"[\d.,\"]*" string))
-                               (apply str (map #(re-matches #"[\d.]*" %) (map str string)))
-                               string))
-            n (clojure.string/split (num string) #"[.]")  t (take 2 n) b (drop 2 n)
-            d (read-string (str (first t) "." (second t) (apply str b)))]
-        (if (zero? (- d (int d))) (int d) d))
-      0)
-    (catch Exception e string)))
+  (let [parse-num (:number spork.util.parsing/parse-defaults)
+        custom-num #(parse-num (clojure.string/replace % "," ""))]
+    (custom-num string)))
 
-;;Converts flat datafile into list of maps using keys from first line (header) of file
-(defn file->map-list [filename & {:keys [del] :or {del "\t"}}]
- (with-open [r (clojure.java.io/reader filename)]
-   (let [firstline (clojure.string/split (first (line-seq r)) (re-pattern del))
-         header (zipmap (range (count firstline)) firstline)
-         lines (pmap #(clojure.string/split % (re-pattern del)) (line-seq r))
-         data (for [line lines] (zipmap (map keyword firstline) line))]
-     (into [] data))))
-
-
+;;Returns a list of maps containing the data from the tab-delimited file
+;;The keys of each map are either the header row of the file if no schema is provided
+;;Otherwise, the keys of each map are what are defined by the schema and are formatted accordingly
+(defn file->map-list [filename & {:keys [schema] :or {schema {}}}]
+  (as-> filename it
+        (spork.util.table/tabdelimited->records it :schema schema)
+        (into [] it)))
 
 ;;Creates a stacked-xy-chart from data in supplied file
 ;; -filename is the filepath
@@ -49,8 +39,8 @@
 ;; -view will display the chart on the screen
 ;; Returns the jfree char
 (defn file->stacked-xy-chart [filename groupby-key startfn endfn yfn
-                              & {:keys [title x-label y-label] :or {title "" x-label "Time" y-label "Quantity"}}]
-  (let [ds (jf/->ds (file->map-list filename) groupby-key yfn startfn endfn)
+                              & {:keys [title x-label y-label schema] :or {title "" x-label "Time" y-label "Quantity" schema {}}}]
+  (let [ds (jf/->ds (file->map-list filename :schema schema) groupby-key yfn startfn endfn)
         chart (jf/stacked-xy-chart ds :title title :x-label x-label :y-label y-label)]
     chart))
 
@@ -60,12 +50,13 @@
 ;;Cont will make the changes between time periods continous, otherwise the changes will be discrete
 ;;Returns JFreeChart object
 (defn file->sand-charts [filename key startfn endfn yfn 
-                         & {:keys [save view title x-label y-label] 
-                            :or {save false view true title "Sand Chart" x-label "Time" y-label "Quantity"}}]
-  (let [prefix (first (clojure.string/split (last (clojure.string/split filename #"[/|\\]")) #"[.]"))
+                         & {:keys [save view title x-label y-label schema] 
+                            :or {save false view true title "Sand Chart" x-label "Time" y-label "Quantity" schema {}}}]
+  (let [prefix (clojure.string/replace (spork.util.io/fname filename) (str "." (spork.util.io/fext filename)) "")
+        outfile (clojure.string/replace filename (str "." (spork.util.io/fext filename)) "-SandChart.png")
         chart (file->stacked-xy-chart filename key startfn endfn yfn 
-                :save save :view view :title (str prefix " - Sand Chart") :x-label x-label :y-label y-label)] 
-    (when save (incanter.core/save chart (str (apply str (take (- (count filename) 4) filename)) "-SandChart.png")))
+                :save save :view view :title (str prefix " - Sand Chart") :x-label x-label :y-label y-label :schema schema)] 
+    (when save (incanter.core/save chart outfile))
     (when view (incanter.core/view chart))
     chart))
 
@@ -73,66 +64,15 @@
 ;;Function for Sand Charts from formatted demand file
 ;;If demand file does not contain strength (people) per src, a supply file can be supplied to pull the data from
 (defn demand-file->sand-charts [demandfile & {:keys [save view cont supplyfile] :or {save false view true cont true}}]
-  (let [startfn #(read-num (:StartDay %))
-        endfn #(+ (read-num (:StartDay %)) (read-num (:Duration %)))]
+  (let [startfn #(:StartDay %)
+        endfn #(+ (:StartDay %) (:Duration %))
+        fromDemand {:SRC :text :DemandGroup :text :Quantity read-num :StartDay read-num :Duration read-num :People read-num}
+        fromSupply {:SRC :text :DemandGroup :text :Quantity read-num :StartDay read-num :Duration read-num :Strength read-num}]
     (if (not= nil supplyfile)
       (let [supply (set (for [m (file->map-list supplyfile)] [(:SRC m) (:Strength m)]))
             strmap (zipmap (map first supply) (map second supply))]
-        (file->sand-charts demandfile "DemandGroup" startfn endfn #(* (read-num (:Quantity %)) (max 1 (read-num (get strmap (:SRC %))))) :save true))
-      (file->sand-charts demandfile "DemandGroup" startfn endfn #(* (read-num (:Quantity %)) (max 1 (read-num (:People %)))) :save true))))
+        (file->sand-charts demandfile "DemandGroup" startfn endfn #(* (:Quantity %) (max 1 (get strmap (:SRC %)))) :schema fromSupply :save true))
+      (file->sand-charts demandfile "DemandGroup" startfn endfn #(* (:Quantity %) (max 1 (:People %))) :schema fromDemand :save true))))
 ;;; ===============================================================================
 
-
-
-
-;;; ========== OLD, DON'T USE =======================================================================================
-;;Returns a map containing a list of times and a list of values at the time
-;;The nth value in times corresponds to the nth value in y, ie xy-pair = [(nth times i) (nth y i)]
-;;The global-start/end values need to be calculated from the entire data set
-;;times and y should ALWAYS be the same length
-(comment
-  (defn coords [map-list global-start global-end ykeyfn startkeyfn endkeyfn]
-    (let [times (range global-start global-end)
-          y (map #(y-at-time map-list % ykeyfn startkeyfn endkeyfn) times)]
-      {:times times :y y}))
-  
-  ;;Returns a list of maps with groupkey as key, times as list of times and y-values as list of y-values at time
-  (defn get-plot-data [grouped-map-list key startkeyfn endkeyfn ykeyfn]
-    (for [k (keys grouped-map-list) :let [start (global-start grouped-map-list startkeyfn) end (global-end grouped-map-list endkeyfn)]]
-      (conj {(keyword key) k} (coords (get grouped-map-list k) start end ykeyfn startkeyfn endkeyfn))))
-  
-  ;;Repeates val n times
-  (defn rep [val n]
-    (map #(if (or true %) val) (range n)))
-  
-  ;;Returns a JFreeChartObject reperenting the sand chart
-  ;;grouped-map-list is the result of calling group-by on the list of maps from the read-in data
-  ;;key is the variable name from the input file that the data should be grouped by (String, not keyword)
-  ;;startfn, endfn, and yfn are all functions that return numerical values when called on a data map
-  ;;The optional cont keyword argument will determine in the chart should be continous (true) or discrete (false)
-  (defn build-sand-chart [grouped-map-list key startfn endfn yfn & {:keys [title] :or {title ""}}]
-    (let [data (get-plot-data grouped-map-list key startfn endfn yfn)
-          start (global-start grouped-map-list startfn)
-          end (global-end grouped-map-list endfn)
-          keys (keys grouped-map-list)
-          length (count (:times (first data))) ;should be equal to end-start
-          groupby (flatten (map #(flatten (if (or true %) keys)) (range length)))
-          times (flatten (for [t (range start end)] (rep t (count keys))))
-          quantities (flatten (for [t (range 0 length)] (map #(nth (:y %) t) data)))
-          chart (if (= true true)
-                  (incanter.charts/stacked-area-chart times quantities :group-by groupby 
-                    :legend true :x-label "Time Period" :y-label "Quantity" :title title)
-                  (incanter.charts/stacked-bar-chart times quantities :group-by groupby 
-                    :legend true :x-label "Time Period" :y-label "Quantity" :title title))
-          invisible (java.awt.Font. "Tahoma" 0 0)
-          visible (java.awt.Font. "Tahoma" 0 8)]
-      (.setMaximumCategoryLabelLines (.getDomainAxis (.getCategoryPlot chart)) 5) ;;can change for readability 
-      (doseq [i (range start (inc end))]
-        ;;need to set some of the tick label to be invisible to make it more readable
-        ;;if the label overlap, they will get replaceced by "..."
-        (if (zero? (rem i (Math/floor (/ end 20)))) ;Maximum of 20 label tick points, can change if need more
-          (.setTickLabelFont (.getDomainAxis (.getCategoryPlot chart)) i visible)
-          (.setTickLabelFont (.getDomainAxis (.getCategoryPlot chart)) i invisible)))
-      chart)))
-;;; =================================================================================================================
 
--- src/demand_builder/peak.clj
+++ src/demand_builder/peak.clj
@@ -1,11 +1,11 @@
 (ns demand_builder.peak
   (:require [incanter core charts]
-            [demand_builder [chart :as c]]
             [demand_builder [jfreeoverride :as jf]]
-            [spork.util [table :as t]])
+            [spork.util table [io :as io]])
   (:import [org.jfree.chart.annotations CategoryAnnotation CategoryLineAnnotation TextAnnotation]
            [org.jfree.chart.plot Plot]
            [org.jfree.chart.axis CategoryAnchor]))
+
 ;;Name space for building peak demand stacked bar charts. 
 ;; ***Input file will be different from the output from demand builder (demand output) ***
 ;;    The expected file format contains columns for SRC, AC, RC, NG, and peak
@@ -20,16 +20,16 @@
     (doseq [m map-list 
             :let [peak (:peak m)
                   demand (apply + [(:AC m) (:RC m) (:NG m)])
-                  color (if (< demand peak) (java.awt.Color. 205 0 0) (java.awt.Color. 0 205 0))]]
+                  color (if (< demand peak) (java.awt.Color/red) (java.awt.Color/black))]]
       (.addAnnotation (.getCategoryPlot chart) (jf/annotation chart (:SRC m) peak :paint color))))) 
 
 ;;Creates the stacked bar plot and adds the peak lines for each SRC
 ;;Returns the chart object
 (defn build-peak-chart [map-list & {:keys [title]}]
   (let [chart (incanter.charts/stacked-bar-chart 
-                (flatten (map #(c/rep (:SRC %) 3) map-list)) ;; categories
+                (flatten (map #(repeat 3 (:SRC %)) map-list)) ;; categories
                 (flatten (for [m map-list] [(:AC m) (:RC m) (:NG m)])) ;;supply (y-values)
-                :group-by (flatten (c/rep ["AC" "RC" "NG"] (count (set (map #(:SRC %) map-list))))) ;;group-by
+                :group-by (flatten (repeat (count (set (map #(:SRC %) map-list))) ["AC" "RC" "NG"])) ;;group-by
                 :legend true :x-label "SRC" :y-label "Number of units" :title title)
         font (java.awt.Font. "Tahoma" 0 10)
         srcs (map #(:SRC %) map-list)]
@@ -43,8 +43,8 @@
 ;; save keyword as true will save file in same directory as input file as filename-peaks.png
 ;; view keyword as true will display the plot
 (defn file->peak-chart [filename & {:keys [save view] :or {save false view false}}]
-  (let [chart (build-peak-chart (c/file->map-list filename) :title (str "Peak Demands: " (last (clojure.string/split filename #"[\\|/]")))) 
-        out (str (apply str (take (- (count filename) 4) filename)) "-peaks.png")]
+  (let [chart (build-peak-chart (c/file->map-list filename) :title (str "Peak Demands: " (clojure.string/replace (io/fname filename) (str "." (io/fext filename)) ""))) 
+        out (clojure.string/replace filename (str "." (io/fext filename)) "-peaks.png")]
     (when save (incanter.core/save chart out))
     (when view (incanter.core/view chart))
     chart))

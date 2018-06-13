d2b0d65264cdeefd5ee691b6a48c7cb07f33cdf0
mpavlak1 <mpavlak@gmu.edu>
2018-06-13 15:58:33 -0400

    seesaw for gui

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[seesaw "1.4.2" :exclusions [org.clojure/clojure]]
+                 [org.clojure/clojure "1.9.0"]
                  [spork "0.2.1.0-SNAPSHOT"]
                  [proc "0.2.6-SNAPSHOT"]
                  ;;should be updated to 1.9.3

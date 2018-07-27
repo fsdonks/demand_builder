ac59188a03d7ead47073b6366b66df0f4e721572
mpavlak1 <michael.m.pavlak.civ@mail.mil>
2018-07-27 08:04:39 -0400

    updated gui

--- src/demand_builder/gui.clj
+++ src/demand_builder/gui.clj
@@ -27,6 +27,10 @@
   (let [t (JTextArea. txt)]
     (do (.setRows t 100) (.setSize t x y) (.setLineWrap t true)) t))
 
+(defn y-panel [] (let [p (javax.swing.JPanel.)] (.setLayout p (javax.swing.BoxLayout. p javax.swing.BoxLayout/Y_AXIS))  p))
+(defn text-field [& {:keys [size] :or {size 7}}] (let [tf (JTextField.)] (.setColumns tf size) tf))
+(defn show-frame [f p & {:keys [x y] :or {x 100 y 100}}] (doto f (.add p) (.setSize x y) (.setVisible true)) nil)
+
 ;;Creates gui with txt (txt = .getStackTrace of exception)
 (defn error-gui [txt]
   (let [f (frame :title "Error StackTrace") p (y-panel) t (text-area txt)]
@@ -40,10 +44,6 @@
                                 (catch Exception e
                                   (do (error-gui (.getMessage e)) (throw (ex-info (.getMessage e) {:input 42})))))))
     b))
-
-(defn y-panel [] (let [p (javax.swing.JPanel.)] (.setLayout p (javax.swing.BoxLayout. p javax.swing.BoxLayout/Y_AXIS))  p))
-(defn text-field [& {:keys [size] :or {size 7}}] (let [tf (JTextField.)] (.setColumns tf size) tf))
-(defn show-frame [f p & {:keys [x y] :or {x 100 y 100}}] (doto f (.add p) (.setSize x y) (.setVisible true)) nil)
 
 (defn vec->line [v] (str (apply str (map #(str % "\t") v)) "\n"))
 (defn spit-tsv [filename lines header]
@@ -101,19 +101,19 @@
         frame (frame :title "Phase Shifter")
         mainpanel (JPanel.) scenariopanel (y-panel) phasepanel (y-panel) deltapanel (y-panel) header (y-panel)
         objs (doall (for [i (range 10)] {:scenario (text-field :size 10) :phase (text-field) :change (text-field)}))
-        panels [scenariopanel phasepanel deltapanel]]        
+        panels [scenariopanel phasepanel deltapanel]]
     (doall (map #(.setText ((first %) (first objs)) (second %)) [[:scenario "DemandGroup"] [:phase "Phase"] [:change "Change"]]))
     (doseq [o objs] (.add scenariopanel (:scenario o)) (.add phasepanel (:phase o)) (.add deltapanel (:change o)))
     (doseq [p panels] (.add mainpanel p) (.add frame mainpanel))
     (.add header (label (str "<html>" (data->str scenarios "Scenarios: ") "<br>" (str "<br>" (data->str phases "Phases: ")))))
     (.add mainpanel header)
     (.add mainpanel (fn->button (fn [] (phases->gui file)) "Phase Timings"))
-    (.add mainpanel (fn->button (fn [] (let [lines (doall (for [o objs] [(.getText (:scenario o)) (.getText (:phase o)) 
+    (.add mainpanel (fn->button (fn [] (let [lines (doall (for [o objs] [(.getText (:scenario o)) (.getText (:phase o))
                                                                          (let [t (.getText (:change o))] (try (c/read-num t) (catch Exception e t)))]))]
                                          (spit (str root "changes.txt") (reduce str (map #(str (first %) "\t" (second %) "\t" (last %) "\n") (filter #(not= "" (first %)) lines))))
                                          (ps/->output-new-phases file (ps/read-header file) (str root "changes.txt") file)
                                          (clojure.java.io/delete-file (str root "changes.txt"))
-                                         (.setVisible frame false))) "Shift Phases"))   
+                                         (.setVisible frame false))) "Shift Phases"))
     (show-frame frame mainpanel :x 432 :y 276)))
 
 ;;This gui will allow users to enter values to shift the start/duration of demand groups in a demand file
@@ -140,7 +140,7 @@
 ;;Set Working Dir, Set Existing Dir, Shift Phases, Shift DemandGroup, Sand Chart 
 (defn ->buttons [lbl]
   (map #(fn->button (first %) (second %)) 
-    [[#(let [root (file-choice->root (choose-file))]
+    [[#(let [root (file-choice->root (select-file))]
          (when (io/fexists? root)
            (.setText lbl (root->outputdir root)) (confirm-input-map root)))  "Set Working Directory"]
      [#(let [f (choose-file)] (when (pos? (count (apply concat (vals (inputs-used (file->pdir f))))))
@@ -150,9 +150,8 @@
      [#(c/demand-file->sand-charts (outdir->demandfile (.getText lbl)) :view true :save false) "Sand Chart"]]))
 
 (defn main-gui []
-  (let [f (frame :title "Demand Builder")
-        p (y-panel)
-        rdl (label "No working directory set")]
+  (let [f (frame :title "Demand Builder") p (y-panel) rdl (label "No working directory set")]
     (doseq [c (flatten (list rdl (->buttons rdl)))] (.add p c))
     (show-frame f p :x 366 :y 200)))
+
 

37ce05f5bcf1852ff6ed0f71d26d993254bf6d7a
tom <tom@the-cloud>
2018-09-17 22:33:06 +0000

    Minimizing deps

--- project.clj
+++ project.clj
@@ -3,12 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[seesaw "1.4.2" :exclusions [org.clojure/clojure]]
+  :dependencies [[seesaw "1.4.5" :exclusions [org.clojure/clojure]]
                  [org.clojure/clojure "1.9.0"]
                  [spork "0.2.1.0-SNAPSHOT"]
                  [proc "0.2.6-SNAPSHOT"]
                  ;;should be updated to 1.9.3
-                 [joinr/incanter "1.9.3-SNAPSHOT"]]
+                 [joinr/incanter-core "1.9.3-SNAPSHOT"]
+                 [joinr/incanter-charts "1.9.3-SNAPSHOT"]]
   :source-paths ["src" "../spork/src"]
   :main demand-builder.main
   :aot [demand-builder.main]

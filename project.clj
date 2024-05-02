0ab0e14c6292c22c95d99363b9d3d3e01ed712dc
tom <tom@the-cloud>
2024-05-02 16:44:49 +0000

    Version bump.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject demand_builder "0.1.3-SNAPSHOT"
+(defproject demand_builder "0.1.4-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
